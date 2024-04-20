71e5b34e56f9798f4fc930ba7a68f0715c709055
rechenberger@hiberno.de <rechenberger@hiberno.de>
2024-04-20 18:04:26 +0200

    add start script
    Create a PowerShell start script that calls itself as administrator, if these rights are not yet available. It then executes the provisioning of vagrant.
    
    KBOX-25: Starten der KrecBox per Script

---
+++ startKrecBox.ps1
@@ -0,0 +1,51 @@
+function Check-RunAsAdministrator {
+    param([String]$VagrantPath)
+
+    # get current user context
+    $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+
+    # check user is admin
+    if ($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+    {
+        Write-Host "Script is running with Administrator privileges!"
+    }
+    else
+    {
+        # create a new elevated process to start PowerShell
+        $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+        # specify the current script path and name as a parameter
+        $ElevatedProcess.Arguments = "& '" + $Script:MyInvocation.MyCommand.Path + "' '$VagrantPath'"
+
+        # set the process to elevated
+        $ElevatedProcess.Verb = "runas"
+
+        # start the new elevated process
+        [System.Diagnostics.Process]::Start($ElevatedProcess)
+
+        # exit from current process
+        Exit
+    }
+}
+# check if location parameter is given and set path for vagrant
+$VagrantPath
+if ($( $args.Count ) -lt 1)
+{
+    $VagrantPath = Get-Location
+}
+else
+{
+    $VagrantPath = $args[0]
+}
+
+# check if script is running as administrator, cause vagrant with provider hyper-v need to start as administrator
+Check-RunAsAdministrator $VagrantPath
+
+# go to vagrant path
+Set-Location $VagrantPath
+
+# start vagrant with provision
+vagrant up --provision
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
